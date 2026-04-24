b3553fd47287e2c6c952c79c19b9df15427041ab
MrHanestein <giddydanny002@gmail.com>
2026-04-24 13:49:18 +0100

    Added L1,L2,L3 constructs with buckets using the documentation

--- cdk-start/lib/cdk-start-stack.ts
+++ cdk-start/lib/cdk-start-stack.ts
@@ -1,16 +1,61 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+//import { Duration } from 'aws-cdk-lib';
+import { CfnOutput, Duration } from "aws-cdk-lib";
+import { Bucket, CfnBucket } from "aws-cdk-lib/aws-s3";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+class L3Bucket extends Construct {
+  constructor(scope: Construct, id: string, expiration: number) {
+    super(scope, id);
+
+    new Bucket(this, "L3Bucket", {
+      lifecycleRules: [
+        {
+          expiration: Duration.days(expiration),
+        },
+      ],
+    });
+  }
+}
 
 export class CdkStartStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create an S3 Bucket - Storage
+    new CfnBucket(this, "MyL1Bucket", {
+      lifecycleConfiguration: {
+        rules: [
+          {
+            expirationInDays: 1,
+            status: "Enabled",
+          },
+        ],
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkStartQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const duration = new cdk.CfnParameter(this, "durration", {
+      default: 6,
+      minValue: 1,
+      maxValue: 12,
+      type: "Number",
+    });
+
+    const myL2Bucket = new Bucket(this, "MyL2Bucket", {
+      lifecycleRules: [
+        {
+          expiration: Duration.days(duration.valueAsNumber),
+        },
+      ],
+      // Run on destroy command to cleanly wipe
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    new CfnOutput(this, "MyL2BucketName", {
+      value: myL2Bucket.bucketName,
+    });
+
+    new L3Bucket(this, "MyL3Bucket", 3);
   }
 }
